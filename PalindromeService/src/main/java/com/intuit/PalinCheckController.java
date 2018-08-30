52c7704700bcde46e1a0b8ce7eb37031d62cee53
prabhu-durasoft <iamprabhu@yahoo.com>
2018-08-30 11:14:13 +0530

    Day04

---
+++ PalindromeService/src/main/java/com/intuit/PalinCheckController.java
@@ -0,0 +1,20 @@
+package com.intuit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PalinCheckController {
+
+	@GetMapping("/palincheck/{word}")
+	public String check(@PathVariable String word) {
+		String[] wordArr = word.split("");
+		StringBuilder reversedWord = new StringBuilder(""); 
+		for (int i = wordArr.length - 1; i > -1; i--) {
+			reversedWord.append(wordArr[i]);
+		}
+		boolean isPalin = word.equalsIgnoreCase(reversedWord.toString());
+		return isPalin ? "Palindrome" : "Not a Palindrome";
+	}
+}
